fa4f525274ff773bf2cc3f4e4f50aa47e125cd17
Ramnoah002 <ramnoah002@gmail.com>
2026-06-30 00:38:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        window.open("https://wa.me/254704684998?text=Hello%20Lavine's%20Apparel,%20I'd%20like%20to%20place%20an%20order.", "_blank");
+    });
+});
